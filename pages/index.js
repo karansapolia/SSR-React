77a17bea379f04dcd5c3041578f5d2c7aa548a8e
x <x@localhost.localdomain>
2019-01-16 18:32:40 +0530

    feat(pags/index.js) Experimenting with lists

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,54 @@
 import React from 'react';
-import DigitalClock from '../src/DigitalClock';
+import axios from 'axios';
 
 class Index extends React.Component {
+
+    static async getInitialProps() {
+        var promise = axios.get('http://localhost:4000/speakers').
+            then(response => {
+                return {
+                    hasErrored: false,
+                    speakerData: response.data
+                };
+            })
+            .catch(error => {
+                return {
+                    hasErrored: true,
+                    message: error.message
+                }
+            });
+        return promise;
+    }
+
+
+
     constructor(props) {
         super(props);
         this.state = {
-            time: new Date().toLocaleString()
+            hasErrored: props.hasErrored,
+            message: props.message,
+            speakerData: props.speakerData
         }
     }
 
-    tick() {
-        this.setState(() => {
-            return ({
-                time: new Date().toLocaleString()
-            });
-        });
+    componentDidMount() {
+
     }
 
-    componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
+    componentWillUnmount() {
+
     }
 
     render() {
-        return <DigitalClock time={this.state.time}></DigitalClock>
+        return (
+            <ul>
+                {this.state.speakerData.map((speaker) =>
+                <li key={speaker.id}>
+                    {speaker.firstName} {speaker.lastName}
+                </li>
+                )}
+            </ul>
+        )
     }
 }
 
