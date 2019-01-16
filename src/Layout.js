3cb5434da89726309428c9737f3dafcfec9c6dc2
Karan Sapolia <sapoliakaran@gmail.com>
2019-01-16 22:58:58 +0530

    feat(css, _app, _document, bootstrap): Adding bootstrap and componentizing UI

---
+++ src/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/site.css';
+
+export class Layout extends React.Component {
+    render() {
+        const {children} = this.props;
+
+        return (
+            <React.Fragment>
+                <Header/>
+                <Menu/>
+                {children}
+                <Footer/>
+            </React.Fragment>
+        );
+    }
+}
